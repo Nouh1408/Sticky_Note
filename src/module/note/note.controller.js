047696f85da1bdc5b06c5ca5f72a7cccf03937a7
Nouh1408 <ahmedinouh@gmail.com>
2025-08-25 08:11:51 +0300

    deleted success

--- src/module/note/note.controller.js
+++ src/module/note/note.controller.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addNote, replacenote, updateAllNote, updateNote } from "./services/note.srvice.js";
+import { addNote, deletednote, replacenote, updateAllNote, updateNote } from "./services/note.srvice.js";
 const router = Router()
 router.post('/addnote', addNote)
 router.patch('/updateNote/:noteId', updateNote)
 router.put('/replace/:noteId', replacenote)
 router.patch('/updateAll', updateAllNote)
+router.delete('/deletednote/:noteId', deletednote)
 export default router
--- src/module/note/services/note.srvice.js
+++ src/module/note/services/note.srvice.js
@@ -1,110 +1,155 @@
-import  jwt  from "jsonwebtoken"
-import { User } from "../../../db/model/user.model.js"
-import {Note} from '../../../db/model/notes.model.js'
+import jwt from "jsonwebtoken";
+import { User } from "../../../db/model/user.model.js";
+import { Note } from "../../../db/model/notes.model.js";
 
-export const addNote = async (req,res,next)=>{
-    try {
-        const {title,content, userId}= req.body
-        const{authorization} = req.headers
-        const decoded = jwt.verify(authorization,"usertoken")
-        const user = await User.findById(decoded.id)
-        if(!user){
-            return res.status(404).json({message:"user not found"})
-        }
-        if(userId !== decoded.id){
-            return res.status(409).json({message:"user can't add note"})
-        }
-        const note = await Note.create({title,content,userId})
-        return res.status(201).json({message:"cote created", note} )
-        
-    } catch (error) {
-        return res.status(500).json({message:"server error", error, message:error.message})
+export const addNote = async (req, res, next) => {
+  try {
+    const { title, content, userId } = req.body;
+    const { authorization } = req.headers;
+    const decoded = jwt.verify(authorization, "usertoken");
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
     }
-}
-
-export const updateNote = async (req,res,next)=>{
-    try {
-       
-        const{authorization} = req.headers
-        const decoded = jwt.verify(authorization,"usertoken")
-        const user = await User.findById(decoded.id)
-        if(!user){
-            return res.status(404).json({message:"user not found"})
-        }
-        const {noteId} = req.params
-        const note = await Note.findById(noteId)
-
-        if(!noteId){
-            return res.status(404).json({message:"note not found"})
-        }
-        if(note.userId.toString() !== decoded.id){
-            return res.status(404).json({message:"not note owner"})
-        }
-        const updatedNote = await Note.findByIdAndUpdate(noteId,{...req.body}, {new:true})
-        
-        return res.status(201).json({message:"note updated", updatedNote} )
-        
-    } catch (error) {
-        return res.status(500).json({message:"server error", error, message:error.message})
+    if (userId !== decoded.id) {
+      return res.status(409).json({ message: "user can't add note" });
     }
-}
-export const replacenote = async (req,res,next)=>{
-    try {
-       
-        const{authorization} = req.headers
-        const decoded = jwt.verify(authorization,"usertoken")
-        const user = await User.findById(decoded.id)
-        if(!user){
-            return res.status(404).json({message:"user not found"})
-        }
-        const {noteId} = req.params
-        const note = await Note.findById(noteId)
+    const note = await Note.create({ title, content, userId });
+    return res.status(201).json({ message: "cote created", note });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "server error", error, message: error.message });
+  }
+};
 
-        if(!note){
-            return res.status(404).json({message:"note not found"})
-        }
-        if(!note.userId ){
-            return res.status(400).json({message:"note not have userIf"})
-        }
-        if(note.userId.toString() !== decoded.id){
-            return res.status(404).json({message:"not note owner"})
-        }
-        const newnote = await Note.replaceOne({_id : noteId}, {...req.body}, {new:true})
-        
-        return res.status(201).json({message:"note updated", updatedNote: newnote} )
-        
-    } catch (error) {
-        return res.status(500).json({message:"server error", error, message:error.message})
+export const updateNote = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    const decoded = jwt.verify(authorization, "usertoken");
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
     }
-}
-export const updateAllNote = async (req,res,next)=>{
-    try {
-       
-        const{authorization} = req.headers
-        const decoded = jwt.verify(authorization,"usertoken")
-        const user = await User.findById(decoded.id)
-        const userId = decoded.id
-        if(!user){
-            return res.status(404).json({message:"user not found"})
-        }
-        const {title}= req.body
-        if(!title){
-            // throw new Error("title is required", 400)
-            return res.status(404).json({message:"no notes found"})
-        }
-        const notes = await Note.find({userId})
+    const { noteId } = req.params;
+    const note = await Note.findById(noteId);
 
-        if(!notes||notes.length == 0){
-              return res.status(404).json({message:"no notes found"})
-        }
-       
-        const updatedNote = await Note.updateMany({userId}, {
-            $set:{title}
-        })
-        
-        return res.status(201).json({message:"all notes updated", updatedNote} )
-        
-    } catch (error) {
-        return res.status(500).json({message:"server error", error, message:error.message})
+    if (!noteId) {
+      return res.status(404).json({ message: "note not found" });
     }
-}
+    if (note.userId.toString() !== decoded.id) {
+      return res.status(404).json({ message: "not note owner" });
+    }
+    const updatedNote = await Note.findByIdAndUpdate(
+      noteId,
+      { ...req.body },
+      { new: true }
+    );
+
+    return res.status(201).json({ message: "note updated", updatedNote });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "server error", error, message: error.message });
+  }
+};
+export const replacenote = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    const decoded = jwt.verify(authorization, "usertoken");
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    const { noteId } = req.params;
+    const note = await Note.findById(noteId);
+
+    if (!note) {
+      return res.status(404).json({ message: "note not found" });
+    }
+    if (!note.userId) {
+      return res.status(400).json({ message: "note not have userIf" });
+    }
+    if (note.userId.toString() !== decoded.id) {
+      return res.status(404).json({ message: "not note owner" });
+    }
+    const newnote = await Note.replaceOne(
+      { _id: noteId },
+      { ...req.body },
+      { new: true }
+    );
+
+    return res
+      .status(201)
+      .json({ message: "note updated", updatedNote: newnote });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "server error", error, message: error.message });
+  }
+};
+export const updateAllNote = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    const decoded = jwt.verify(authorization, "usertoken");
+    const user = await User.findById(decoded.id);
+    const userId = decoded.id;
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    const { title } = req.body;
+    if (!title) {
+      // throw new Error("title is required", 400)
+      return res.status(404).json({ message: "no notes found" });
+    }
+    const notes = await Note.find({ userId });
+
+    if (!notes || notes.length == 0) {
+      return res.status(404).json({ message: "no notes found" });
+    }
+
+    const updatedNote = await Note.updateMany(
+      { userId },
+      {
+        $set: { title },
+      }
+    );
+
+    return res.status(201).json({ message: "all notes updated", updatedNote });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "server error", error, message: error.message });
+  }
+};
+export const deletednote = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    const decoded = jwt.verify(authorization, "usertoken");
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    const { noteId } = req.params;
+    const note = await Note.findById(noteId);
+
+    if (!note) {
+      return res.status(404).json({ message: "note not found" });
+    }
+     if (!note.userId) {
+      return res.status(400).json({ message: "note does not have a user" });
+    }
+    if (note.userId.toString() !== decoded.id) {
+      return res.status(404).json({ message: "not owner" });
+    }
+   
+
+    const deletednote = await Note.findByIdAndDelete(noteId)
+
+    return res.status(201).json({ message: "note deleted", deletednote });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "server error", error, message: error.message });
+  }
+};
